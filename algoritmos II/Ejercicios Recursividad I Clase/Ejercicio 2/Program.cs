5864145092152c9f4cf39e9ad83d61e9ac2e3dcb
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-06 11:52:30 -0500

    Create Program.cs

---
+++ algoritmos II/Ejercicios Recursividad I Clase/Ejercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int numero;
+        Console.WriteLine("ingrese el numero");
+        numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("el numero es: {0}", numero);
+        for (int i = numero; i != 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
