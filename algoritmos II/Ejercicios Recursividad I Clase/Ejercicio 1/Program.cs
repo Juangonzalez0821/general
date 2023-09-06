38d3ae823bc2630490a73bc14f26991819ac898b
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-06 11:56:32 -0500

    Create Program.cs

---
+++ algoritmos II/Ejercicios Recursividad I Clase/Ejercicio 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static int Power(int baseNum, int exponent)
+    {
+        if (exponent == 0)
+            return 1;
+
+        return baseNum * Power(baseNum, exponent - 1);
+    }
+
+    static void Main(string[]args)
+    {
+        int baseNum = 2;
+        int exponent = 5;
+        int result = Power(baseNum, exponent);
+        Console.WriteLine($"{baseNum} elevado a la potencia {exponent} es: {result}");
+    }
+}
