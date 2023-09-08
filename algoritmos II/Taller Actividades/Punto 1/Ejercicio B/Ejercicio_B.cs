52bc486a6f569b4b04daa9e4a45382b780aa1450
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 12:40:42 -0500

    Create Ejercicio_B.cs

---
+++ algoritmos II/Taller Actividades/Punto 1/Ejercicio B/Ejercicio_B.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Ejercicio_B
+{
+    static void Main()
+    {
+        double suma = 0;
+        double promedio = 0;
+        Console.WriteLine("Calcular el promedio de 10 números:");
+        Console.WriteLine("¿Cuántos números va a ingresar?");
+        int l = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= l; i++)
+        {
+            Console.WriteLine("Ingrese el número {0}", i);
+            double numero = double.Parse(Console.ReadLine());
+            suma = suma + numero;
+        }
+        promedio = suma / l;
+        Console.WriteLine("El promedio de los números es: {0}", promedio);
+        Console.WriteLine("Presione cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+}
