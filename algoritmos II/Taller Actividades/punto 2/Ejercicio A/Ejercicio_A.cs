2bdc3c899f771c262796e51bb8060e32b615fdbd
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 14:02:02 -0500

    Create Ejercicio_A.cs

---
+++ algoritmos II/Taller Actividades/punto 2/Ejercicio A/Ejercicio_A.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Ejercicio_A
+{
+    static void Main()
+    {
+        int numero;
+        int suma = 0;
+        Console.WriteLine("Sumar números hasta que se ingrese un cero:");
+        do
+        {
+            Console.WriteLine("Ingresa un número (ingresa 0 para salir): ");
+            numero = int.Parse(Console.ReadLine());
+            if (numero != 0)
+            {
+                suma =suma+ numero;
+            }else
+            {
+                Console.WriteLine("Saliendo del programa...");
+            }
+        } while (numero != 0);
+        Console.WriteLine("la suma es: {0}", suma);
+        Console.WriteLine("Fin del programa.");
+        Console.WriteLine("Presione cualquier tecla para salir.");
+        Console.ReadKey();
+
+    }
+}
+
+
+
+
