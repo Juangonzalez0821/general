eccb44392eebac53cbbb47fee36a52b0d853d161
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 14:43:46 -0500

    hasta p 2 e b

--- algoritmos II/Taller Actividades/punto 2/Ejercicio B/Ejercicio_B.cs
+++ algoritmos II/Taller Actividades/punto 2/Ejercicio B/Ejercicio_B.cs
@@ -5,7 +5,26 @@ class Ejercicio_B
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int numero;
+        Console.WriteLine("Determinar si un número es primo:");
+        Console.WriteLine("Ingresa un numero");
+        numero = int.Parse(Console.ReadLine());
+        if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 )
+        {
+            if (numero == 2 || numero == 3 || numero == 5)
+            {
+                Console.WriteLine("el numero ingresado es primo");
+            }else
+            {
+                Console.WriteLine("no es un numero primo");
+            }
+        }
+        else
+        {
+            Console.WriteLine("el numero ingresado es primo");
+        }
+        Console.WriteLine("Presione cualquier tecla para salir.");
+        Console.ReadKey();
     }
 }
 
