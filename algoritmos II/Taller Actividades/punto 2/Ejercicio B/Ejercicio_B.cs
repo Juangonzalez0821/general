79c7a08cb3665a76f6c6a0dfb1cc9b0a0b08d8d4
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 14:22:36 -0500

    e

---
+++ algoritmos II/Taller Actividades/punto 2/Ejercicio B/Ejercicio_B.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Ejercicio_B
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
