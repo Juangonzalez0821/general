b13c52ed1dd5f57cb089cbc1d3cbba5907dc7c76
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 14:01:42 -0500

    Update Ejercicio_B.cs

--- algoritmos II/Taller Actividades/Punto 1/Ejercicio B/Ejercicio_B.cs
+++ algoritmos II/Taller Actividades/Punto 1/Ejercicio B/Ejercicio_B.cs
@@ -7,7 +7,7 @@ class Ejercicio_B
     {
         double suma = 0;
         double promedio = 0;
-        Console.WriteLine("Calcular el promedio de 10 números:");
+        Console.WriteLine("Promedio de x números: ");
         Console.WriteLine("¿Cuántos números va a ingresar?");
         int l = int.Parse(Console.ReadLine());
 
