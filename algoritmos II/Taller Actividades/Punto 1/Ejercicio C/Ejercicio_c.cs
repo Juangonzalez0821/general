d71826a33cae92ae95429cd1546e0642b5fe23cb
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 13:01:19 -0500

    Create Ejercicio_c.cs

---
+++ algoritmos II/Taller Actividades/Punto 1/Ejercicio C/Ejercicio_c.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Ejercicio_c
+{
+    static void Main()
+    {
+        int n1, //cp, cip;
+        Console.WriteLine("Digite la cantidad de números que quiere contar");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1 ; i <= n ; i++ ){
+            Console.WriteLine("Digite número {0}", i);
+
+        }
+    }
+}
