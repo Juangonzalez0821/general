8c873b06605aef145c0edaa48b03ed7bfd2ee831
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 13:21:46 -0500

    Update Ejercicio_c.cs

--- algoritmos II/Taller Actividades/Punto 1/Ejercicio C/Ejercicio_c.cs
+++ algoritmos II/Taller Actividades/Punto 1/Ejercicio C/Ejercicio_c.cs
@@ -6,13 +6,26 @@ class Ejercicio_c
 {
     static void Main()
     {
-        int n1, //cp, cip;
+        int cp = 0;
+        int cip = 0;
         Console.WriteLine("Digite la cantidad de números que quiere contar");
         int n = int.Parse(Console.ReadLine());
 
         for (int i = 1 ; i <= n ; i++ ){
             Console.WriteLine("Digite número {0}", i);
+            int n1 = int.Parse(Console.ReadLine());
 
+            if (n1 % 2 ==0)
+            {
+                cp++;
+            }else
+            {
+                cip++;
+            }
         }
+        Console.WriteLine("El número de pares es: {0}", cp);
+        Console.WriteLine("El número de impares es: {0}", cip);
+        Console.WriteLine("Presione cualquier tecla para salir.");
+        Console.ReadKey();
     }
 }
