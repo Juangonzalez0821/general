40a239c32454195e677a91419cc745c7e9ba6091
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-09-08 12:40:24 -0500

    Create Ejercicio_A.cs

---
+++ algoritmos II/Taller Actividades/Punto 1/Ejercicio A/Ejercicio_A.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Ejercicio_A
+{
+    static void Main()
+    {
+        Console.Write("ingrese un numero para generar la tabla de multiplicar: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("tabla de multiplicar del {0} : ", numero);
+        for (int i = 1; i <= 10; i++)
+        {
+            int r = numero * i;
+            Console.WriteLine("{0} * {1} = {2}", numero, i, (numero*i));
+        }
+    Console.WriteLine("Presione cualquier tecla para salir.");
+    Console.ReadKey();
+    }
+}
