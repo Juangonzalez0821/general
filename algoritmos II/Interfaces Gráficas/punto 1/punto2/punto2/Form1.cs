e8146b873a687328b507f7e31b1f7b092597a6cf
Juangonzalez0821 <jmgonzalez823@misena.edu.co>
2023-10-19 15:52:08 -0500

    e

--- algoritmos II/Interfaces Gráficas/punto 1/punto2/punto2/Form1.cs
+++ algoritmos II/Interfaces Gráficas/punto 1/punto2/punto2/Form1.cs
@@ -6,5 +6,52 @@ namespace punto2
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //obtener los valores ingresados
+                string nombre = textBox3.Text;
+                int edad = int.Parse(textBox1.Text);
+                int cedula = int.Parse(textBox2.Text);
+
+                //mostrar en el label
+                label5.Text = nombre;
+                label6.Text = edad.ToString();
+                label7.Text = cedula.ToString();
+            }
+            catch (FormatException)
+            {
+                //mostrar la excepcion
+                MessageBox.Show("ingrese un numero valido para la edad o la cedula");
+            }         
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
